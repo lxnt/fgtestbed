6f21676e76d6fcb823358bd5b1ed27842753ba26
Alexander Sabourenkov <screwdriver@lxnt.info>
2012-04-24 16:53:37 +0400

    add licenses

--- fgtestbed.py
+++ fgtestbed.py
@@ -10,6 +10,30 @@
 # a picture of giant bug is engraved on its side
 # ...
 # lxnt cancels Store item in stockpile: interrupted by bug
+"""
+https://github.com/lxnt/fgtestbed
+Copyright (c) 2012-2012 Alexander Sabourenkov (screwdriver@lxnt.info)
+
+This software is provided 'as-is', without any express or implied
+warranty. In no event will the authors be held liable for any
+damages arising from the use of this software.
+
+Permission is granted to anyone to use this software for any
+purpose, including commercial applications, and to alter it and
+redistribute it freely, subject to the following restrictions:
+
+1. The origin of this software must not be misrepresented; you must
+not claim that you wrote the original software. If you use this
+software in a product, an acknowledgment in the product documentation
+would be appreciated but is not required.
+
+2. Altered source versions must be plainly marked as such, and
+must not be misrepresented as being the original software.
+
+3. This notice may not be removed or altered from any source
+distribution.
+
+"""
 
 import sys, time, math, struct, io, ctypes, zlib, ctypes
 import collections, argparse, traceback, os, types, mmap
--- raw.py
+++ raw.py
@@ -1,5 +1,29 @@
 #!/usr/bin/python3.2
 # -*- encoding: utf-8 -*-
+"""
+https://github.com/lxnt/fgtestbed
+Copyright (c) 2012-2012 Alexander Sabourenkov (screwdriver@lxnt.info)
+
+This software is provided 'as-is', without any express or implied
+warranty. In no event will the authors be held liable for any
+damages arising from the use of this software.
+
+Permission is granted to anyone to use this software for any
+purpose, including commercial applications, and to alter it and
+redistribute it freely, subject to the following restrictions:
+
+1. The origin of this software must not be misrepresented; you must
+not claim that you wrote the original software. If you use this
+software in a product, an acknowledgment in the product documentation
+would be appreciated but is not required.
+
+2. Altered source versions must be plainly marked as such, and
+must not be misrepresented as being the original software.
+
+3. This notice may not be removed or altered from any source
+distribution.
+
+"""
 
 import os, os.path, glob, sys, xml.parsers.expat, time, re, argparse
 import traceback, stat, copy, struct, math, mmap, pprint, ctypes, weakref
