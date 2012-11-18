159911ac85b7c39880fe6c78ae738d248d180afb
Alexander Sabourenkov <screwdriver@lxnt.info>
2012-11-18 23:45:50 +0400

    add dependency builder; replace launcher scripts with a single one

---
+++ build-dep.sh
@@ -0,0 +1,87 @@
+#!/bin/sh -e
+
+
+WORK=`realpath $1` || true
+PREFIX=`realpath $2` || true
+PDIR=`realpath ./fgtest.py` || true
+PDIR=`dirname $PDIR`
+WHAT=$3
+
+if [ -z "${PREFIX}" -o -z "${WORK}" -o -z "${PDIR}" ]
+then 
+    echo "Usage: $0 work/dir dest/prefix [sdl|py|all]"
+    echo "Please run this from the fgtestbed directory."
+    exit 1 
+fi
+
+export CFLAGS="-g $CFLAGS"
+export PATH=$PREFIX/bin:$PATH
+
+if [ ! -d $WORK/pyopengl ]
+then
+    echo "Cloning pyopengl repository"
+    bzr branch lp:pyopengl $WORK/pyopengl
+fi
+
+if [ ! -d $WORK/pgreloaded ]
+then
+    echo "Cloning pygame2 repository"
+    hg clone https://code.google.com/p/pgreloaded $WORK/pgreloaded
+fi
+
+if [ ! -d $WORK/SDL ]
+then
+    echo "Cloning SDL2 repository"
+    hg clone http://hg.libsdl.org/SDL $WORK/SDL
+fi
+
+if [ ! -d $WORK/SDL_image ]
+then
+    echo "Cloning SDL_image repository"
+    hg clone http://hg.libsdl.org/SDL_image $WORK/SDL_image
+fi
+
+if [ ! -d $WORK/SDL_ttf ]
+then
+    echo "Cloning SDL_ttf repository"
+    hg clone http://hg.libsdl.org/SDL_ttf $WORK/SDL_ttf
+fi
+
+if [ "sdl" = "$WHAT" -o "all" = "$WHAT" ]
+then
+    [ -d $WORK/build-all ] && rm -r $WORK/build-all
+    mkdir -p $WORK/build-all/sdl $WORK/build-all/image $WORK/build-all/ttf
+    
+    echo "Building and installing SDL2"
+    cd $WORK/build-all/sdl
+    $WORK/SDL/configure --prefix $PREFIX && make -j 4 install
+    ln -sf libSDL2.so "${PREFIX}/lib/SDL2.so"
+
+    echo "Building and installing SDL_image"
+    cd $WORK/build-all/image
+    $WORK/SDL_image/configure --prefix $PREFIX && make -j 4 install
+    ln -sf libSDL2_image.so "${PREFIX}/lib/SDL2_image.so"
+    
+    echo "Building and installing SDL_ttf"
+    cd $WORK/build-all/ttf
+    $WORK/SDL_ttf/configure --prefix $PREFIX && make -j 4 install
+    ln -sf libSDL2_ttf.so "${PREFIX}/lib/SDL2_ttf.so"
+fi
+
+if [ "py" = "$WHAT" -o "all" = "$WHAT" ]
+then
+    echo "Installing pyopengl"
+    cd $WORK/pyopengl
+    python3.2 setup.py install --prefix=$PREFIX
+
+    echo "Installing pygame2"
+    cd $WORK/pgreloaded
+    python3.2 setup.py install --prefix=$PREFIX
+fi
+
+(   echo "#!/bin/sh"
+    echo export PGLIBDIR=$PREFIX/lib
+    echo export PYTHONPATH=$PREFIX/lib/python3.2/site-packages:$PDIR/lib/python
+    echo '"$@"' ) >$PDIR/run
+chmod a+x $PDIR/run
+    
--- paths.sh
+++
@@ -1,8 +0,0 @@
-
-
-export PGLIBDIR="${FGTDIR}/lib"
-export PYTHONPATH="${PGLIBDIR}/python3.2/site-packages/"
-
-[ -f "${PGLIBDIR}/SDL2.so" ] || ln -s libSDL2.so ${PGLIBDIR}/SDL2.so
-[ -f "${PGLIBDIR}/SDL2_image.so" ] || ln -s libSDL2_image.so ${PGLIBDIR}/SDL2_image.so
-[ -f "${PGLIBDIR}/SDL2_ttf.so" ] || ln -s libSDL2_ttf.so ${PGLIBDIR}/SDL2_ttf.so
